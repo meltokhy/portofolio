f7399e26d9423720b65733f6e170c7355491d14b
meltokhy <eltokhymedo2015@gmail.com>
2017-09-14 22:25:38 +0200

    Add files via upload

---
+++ .gitignore/test.js
@@ -0,0 +1,10 @@
+var salary = 350 , bonus = 50 , totalSalary = salary + bonus;
+    
+    
+ 
+var   hasDiscount = true ;
+
+if(hasDiscount === true){
+    salary= totalSalary ;
+} 
+Document.getElementsByClassName("contactme").innerHTML = salary ; 
